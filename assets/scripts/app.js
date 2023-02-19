a6f12cdfe4354d47a58c87da7d24b9f6aa3493cf
Katrin Valge <katrin.valge@mail.ee>
2023-02-19 17:30:53 +0200

    Updates.

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+class Movie {
+  title;
+  image;
+  description;
+
+  constructor(title, image, description) {
+    this.title = title;
+    this.image = image;
+    this.description = description;
+  }
+}
